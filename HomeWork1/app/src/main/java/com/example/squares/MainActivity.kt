7b38a0c8e45c4332805b37d4ba2afbca45319072
panda <pandmacs@gmail.com>
2024-10-31 10:58:03 +0300

    fix rememberSaveable + add changing color Squares

--- HomeWork1/app/src/main/java/com/example/squares/MainActivity.kt
+++ HomeWork1/app/src/main/java/com/example/squares/MainActivity.kt
@@ -4,9 +4,9 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.ui.res.colorResource
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
@@ -16,45 +16,41 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.ViewModel
-import com.example.squares.SquaresViewModel
 
-class SquaresViewModel: ViewModel() {
-    var digits = mutableStateListOf<Int>()
-        private set
-
-    fun addSquare() {
-        digits.add(digits.size)
-    }
-}
 
 class MainActivity : ComponentActivity() {
-    private val viewModel: SquaresViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DrawSquares(viewModel)
+            DrawSquares()
         }
     }
 }
 
 @Composable
-fun DrawSquares(viewModel: SquaresViewModel) {
+fun DrawSquares() {
+
+    val cnt = rememberSaveable { mutableIntStateOf(0) }
+
     val cols = if (LocalConfiguration.current.orientation == Configuration.ORIENTATION_PORTRAIT)
         3 else 4
 
@@ -72,13 +68,13 @@ fun DrawSquares(viewModel: SquaresViewModel) {
                     .weight(1f)
                     .fillMaxWidth()
             ) {
-                items(viewModel.digits) {
-                    digit -> Square(digit, if (digit % 2 == 0) colorResource(id = R.color.red) else colorResource(id = R.color.blue))
+                items(cnt.intValue) { index ->
+                    Square(index, if (index % 2 == 0) colorResource(id = R.color.red) else colorResource(id = R.color.blue))
                 }
             }
             Button(
                 onClick = {
-                    viewModel.addSquare()
+                    cnt.intValue++
                 },
                 modifier = Modifier
                     .fillMaxWidth()
@@ -92,14 +88,22 @@ fun DrawSquares(viewModel: SquaresViewModel) {
     }
 }
 
+val ColorSaver = Saver<MutableState<Color>, Int> (save = { it.value.toArgb() }, restore = { mutableStateOf(Color(it)) })
+
 @Composable
 fun Square(number: Int, color: Color) {
+
+    val clr = rememberSaveable(saver = ColorSaver) { mutableStateOf(color) }
+
     Box(
         modifier = Modifier
             .fillMaxWidth()
             .aspectRatio(1f)
             .padding(8.dp)
-            .background(color),
+            .background(clr.value)
+            .clickable {
+                clr.value = if (clr.value == Color.Red) Color.Blue else Color.Red
+            },
         contentAlignment = Alignment.Center
     ) {
         Text(
